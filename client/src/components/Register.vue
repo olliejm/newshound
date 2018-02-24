e33a762b29cc76875ff7e31bb6eb85a67eb38871
ojm <ojm@riseup.net>
2018-02-24 00:04:38 +0000

    fix readme

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,25 +1,33 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="Email" />
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="Password" />
-    <br>
-    <div class="error" v-html="error" ></div>
-    <br>
-    <button
-      @click="register">
-      Register
-    </button>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input
+            type="email"
+            name="email"
+            v-model="email"
+            placeholder="Email" />
+          <br>
+          <input
+            type="password"
+            name="password"
+            v-model="password"
+            placeholder="Password" />
+          <br>
+          <div class="error" v-html="error" ></div>
+          <br>
+          <v-btn class="cyan"
+            @click="register">
+            Register
+          </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
