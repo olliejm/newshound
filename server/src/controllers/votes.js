74e2d65de62dcbdfca92be0ba9def65ae3fbe410
ojm <ojm@riseup.net>
2018-04-21 12:34:59 +0100

    finish voting implementation and several bug fixes

--- server/src/controllers/votes.js
+++ server/src/controllers/votes.js
@@ -1,11 +1,33 @@
-const {User, Response} = require('../models')
+const {Response, Vote} = require('../models')
 
 module.exports = {
   async show (req, res) {
     try {
       const response = await Response.findById(req.params.responseId)
-      const votes = await response.getVotes()
-      res.send(votes)
+      if (!response) {
+        res.status(404).send({
+          error: 'Invalid response ID'
+        })
+      }
+
+      const upVotes = await Vote.count({
+        where: {
+          upVote: true,
+          ResponseId: req.params.responseId
+        }
+      })
+
+      const downVotes = await Vote.count({
+        where: {
+          upVote: false,
+          ResponseId: req.params.responseId
+        }
+      })
+
+      res.send({
+        upVotes: upVotes,
+        downVotes: downVotes
+      })
     } catch (err) {
       console.log(err)
       res.status(500).send({
@@ -15,18 +37,37 @@ module.exports = {
   },
   async update (req, res) {
     try {
-      const user = await User.findById(req.user.id)
       const response = await Response.findById(req.params.responseId)
-      if (response.UserId === user.id) {
+      if (!response) {
+        res.status(404).send({
+          error: 'Invalid response ID'
+        })
+      }
+
+      if (response.UserId === req.user.id) {
         res.status(403).send({
           error: 'You cannot vote on your own post'
         })
       }
 
-      await response.addVote(user, {
-        upVote: req.body.upVote
+      let vote = await Vote.findOne({
+        where: {
+          UserId: req.user.id,
+          ResponseId: req.params.responseId
+        }
       })
-      res.send(await response.getVotes())
+
+      if (!vote) {
+        vote = await Vote.create({
+          upVote: req.body.upVote,
+          UserId: req.user.id,
+          ResponseId: req.params.responseId
+        })
+      } else {
+        vote.upVote = req.body.upVote
+      }
+
+      res.send(vote)
     } catch (err) {
       console.log(err)
       res.status(500).send({
